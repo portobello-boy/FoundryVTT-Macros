370a118fa915c54f8fd43df374747a46fff7fd6a
daniel millson <danielmillson923@gmail.com>
2022-01-20 16:17:14 -0500

    Adding Hex spell

---
+++ Spells/Lvl2/Hex/ApplyEffectHex.js
@@ -0,0 +1,60 @@
+// This macro must have GM permissions to properly execute
+
+console.log("-- APPLY EFFECT HEX  --")
+
+const spellItem = await fromUuid(args[0].itemUuid)
+const hexUuid = args[0].itemUuid
+const stat = args[1]
+const targetToken = args[2]
+
+let hexEffectIds = []
+
+// Apply hooks
+let hookIdRemoveHex = Hooks.on("preDeleteActiveEffect", (effect) => removeHexEffect(effect))
+
+// Create active effect for hex
+const hexEffect = {
+    changes: [{ key: `flags.midi-qol.disadvantage.ability.check.${stat}`, mode: 2, value: 1, priority: 20 }],
+    origin: hexUuid,
+    label: spellItem.name,
+    icon: spellItem.img,
+    duration: {
+        "seconds": 3600, startTime: game.time.worldTime
+    },
+}
+
+let hexEffectId = ""
+const target = canvas.tokens.get(targetToken.id)
+
+// Apply active effect to target token and store effect ID for later
+await target.actor.createEmbeddedDocuments("ActiveEffect", [hexEffect])
+    .then((res) => {
+        hexEffectId = res[0].id
+        hexEffectIds.push(hexEffectId)
+    })
+
+// Create and apply effect to target token
+new Sequence()
+    .effect()
+    .file("modules/JB2A_DnD5e/Library/Generic/Marker/MarkerHorror_03_Regular_Purple_400x400.webm")
+    .attachTo(target)
+    .scale(0.6)
+    .persist()
+    .name(`Hex-${hexEffectId}`)
+    .fadeIn(1500, { ease: "easeOutCubic", delay: 500 })
+    .fadeOut(1500)
+    .opacity(0.7)
+    .belowTokens(true)
+    .play()
+
+// When the effect ends, remove hex effects on the target token
+async function removeHexEffect(effect) {
+    console.log("-- REMOVE HEX EFFECT --")
+
+    // If the effect is not a hex effect, ignore
+    if (!hexEffectIds.includes(effect.id)) return
+
+    Sequencer.EffectManager.endEffects({ name: `Hex-${effect.id}` });
+
+    Hooks.off("preDeleteActiveEffect", hookIdRemoveHex)
+}
---
+++ Spells/Lvl2/Hex/ApplyEffectHexWrapper.js
@@ -0,0 +1,120 @@
+console.log("-- APPLY EFFECT HEX WRAPPER --")
+
+const spellTargets = args[0].targets
+const spellActor = args[0].actor
+const spellItem = await fromUuid(args[0].itemUuid)
+const hexUuid = args[0].itemUuid
+
+let hexEffectIds = []
+
+// Apply hooks
+let hookIdHexDamage = Hooks.on("midi-qol.RollComplete", (workflow) => addHexDamage(workflow))
+
+// Create dialogue popup
+new Dialog({
+  title: 'Choose a damage type',
+  content: `
+    <form class="flexcol">
+      <div class="form-group">
+        <select id="stat">
+          <option value="str">Strength</option>
+          <option value="dex">Dexterity</option>
+          <option value="con">Constitution</option>
+          <option value="int">Intelligence</option>
+          <option value="wis">Wisdom</option>
+          <option value="cha">Charisma</option>
+        </select>
+      </div>
+    </form>
+  `,
+  buttons: {
+    yes: {
+      icon: '<i class="fas fa-bolt"></i>',
+      label: 'Select',
+      callback: async (html) => {
+        const stat = html.find('#stat').val()
+
+        spellTargets.forEach(async (targetToken) => {
+          // If the target token already has hex applied, return
+          if (targetToken.actor.effects._source.find((ae) => ae.origin == hexUuid)) return
+
+          // Call the GM-level Apply-Effect-Hex macro
+          game.macros.getName("Apply-Effect-Hex").execute(args[0], stat, targetToken)
+        })
+      },
+    },
+  }
+}).render(true);
+
+// When an attack/damage roll is completed, apply hex damage to target token
+async function addHexDamage(workflow) {
+  console.log("-- ADD HEX DAMAGE --")
+
+  // If damage is triggered by hex itself, don't apply it
+  if (workflow.itemUuid == hexUuid) return
+
+  // Retrieve damaged targets which have the hex effect
+  const targets = [...workflow.applicationTargets].filter((target) => target.document.actor.data.effects._source.find((ae) => ae.origin == hexUuid))
+
+  // If there are no targets with this hex effect, return
+  if (!targets.length) return
+
+  // If the aggressor is not the caster of this hex effect, return
+  if (workflow.actor.id != spellActor._id) return
+
+  // Retrieve attack card data
+  const itemCardId = workflow.itemCardId
+  let chatMessage = await game.messages.get(itemCardId)
+  let content = await duplicate(chatMessage.data.content)
+
+  // Roll necrotic damage for hex
+  let damageRoll = new Roll(`1d6`).evaluate({ async: false })
+  game.dice3d.showForRoll(damageRoll)
+  targets.forEach((target) => {
+    // Apply damage to token using the Hex spellItem
+    MidiQOL.applyTokenDamage([{ damage: damageRoll.total, type: "necrotic" }], damageRoll.total, new Set([target]), spellItem, new Set())
+  })
+
+  // Prepare roll info for attack card
+  const dataIdCount = (chatMessage.data.content.match(/data-id=/g) || []).length;
+  const cardDamageRow = `	
+    <div class="dice-roll red-dual" data-id="${dataIdCount}" data-group="4" data-type="damage">
+      <div class="br5e-roll-label">Damage - Necrotic (Hex)</div>
+      <div class="dice-result">
+        <div class="dice-formula dice-tooltip">1d6</div>
+          <div class="dice-row tooltips">
+            <div class="tooltip dual-left dice-row-item">
+              <div class="dice-tooltip">
+              <section class="tooltip-part">
+                  <div class="dice">
+                      <header class="part-header flexrow">
+                          <span class="part-formula">1d6</span>
+                          
+                          <span class="part-total">${damageRoll.total}</span>
+                      </header>
+                      <ol class="dice-rolls">
+                          <li class="roll die d6">4</li>
+                      </ol>
+                  </div>
+              </section>
+            </div>
+            </div>
+              </div>
+              <div class="dice-row">
+            <h4 class="dice-total dice-row-item red-damage" data-damagetype="force">
+              <div class="red-base-die inline red-base-damage " data-value="${damageRoll.total}">${damageRoll.total}</div>
+              
+            </h4>
+          </div>
+        
+        </div>
+    </div>`
+  const searchString = /<div class="midi-qol-hits-display"><div class="midi-qol-single-hit-card">/g
+  const replaceString = `${cardDamageRow}<div class="midi-qol-hits-display"><div class="midi-qol-single-hit-card">`
+
+  // Insert hex result into attack card and update
+  content = await content.replace(searchString, replaceString)
+  await chatMessage.update({ content: content })
+
+  Hooks.off("midi-qol.RollComplete", hookIdHexDamage)
+}
